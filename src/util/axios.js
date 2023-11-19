3378f6327e7df372e80ec2fe348772e781df300b
rahmatilla301993 <rahmatilla1993@gmail.com>
2023-11-19 23:06:38 +0500

    changing api url

--- src/util/axios.js
+++ src/util/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = process.env.REACT_APP_API_URL
+const API_URL = 'https://social-media-app1-37568ee6f2fe.herokuapp.com/api'
 
 const instance = axios.create({
     baseURL: API_URL
