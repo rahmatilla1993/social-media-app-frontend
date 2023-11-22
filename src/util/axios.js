38db9f7e4199d58152947c851cb0215df1147aa3
rahmatilla301993 <rahmatilla1993@gmail.com>
2023-11-22 09:43:50 +0500

    change remote server

--- src/util/axios.js
+++ src/util/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = 'http://185.196.213.95:8080/api'
+const API_URL = process.env.REACT_APP_API_URL
 // const API_URL = 'http://localhost:8080/api'
 
 const instance = axios.create({
